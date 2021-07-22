527918d8d51cf46691344d7dc8307e04034737a8
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-07-22 23:43:25 +0530

    added firbase storage hook

---
+++ PixelGram/src/hooks/useStorage.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { projectStorage } from '../firebase/config'
+
+const useStorage = (file) => {
+    const [progress, setProgress] = useState(0);
+    const [error, setError] = useState(null);
+    const [url, setUrl] = useState(null);
+
+    useEffect(() => {
+        //references
+        const storageRef = projectStorage.file(file.name);
+        //As put is an asynchronous function, we keep a track of the % of the file uploaded with the help of .on() when state is changed, this function may run multiple times as the file uploads
+        storageRef.put(file).on('state_changed',(snap) => {
+            let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+            setProgress(percentage);
+        }, (err) => {
+            setError(err);
+        }, async () => {
+            const url = await storageRef.getDownloadURL();
+            setUrl(url);
+        })
+    }, [file]) //file is a dependency here, the function will run everytime the value of file changes
+
+    return { progress, url, error}
+
+}
+
+export default useStorage;
