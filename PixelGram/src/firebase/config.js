4c10914c609ec659907f3b8ec0a69ca52065faff
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-07-24 00:17:27 +0530

    added modal

--- PixelGram/src/firebase/config.js
+++ PixelGram/src/firebase/config.js
@@ -4,18 +4,12 @@ import 'firebase/firestore';
 require('dotenv').config();
 
 var firebaseConfig = {
-    // apiKey: process.env.API_KEY,
-    // authDomain: process.env.AUTH_DOMAIN,
-    // projectId: process.env.PROJECT_ID,
-    // storageBucket: process.env.STORAGE_BUCKET,
-    // messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    // appId: process.env.APP_ID
-    apiKey: "AIzaSyCm8WmMt2CdmK_1WEPcdB4grGyAce8I99s",
-    authDomain: "pixelgram-80e0b.firebaseapp.com",
-    projectId: "pixelgram-80e0b",
-    storageBucket: "pixelgram-80e0b.appspot.com",
-    messagingSenderId: "722674977769",
-    appId: "1:722674977769:web:1e7b7a9082a8be73d89978"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
