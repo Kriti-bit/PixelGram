54eecc7a73421eb7aada5ddf340af102a2329b94
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-07-23 23:48:41 +0530

    added firestore hook and showing images

--- PixelGram/src/firebase/config.js
+++ PixelGram/src/firebase/config.js
@@ -4,12 +4,18 @@ import 'firebase/firestore';
 require('dotenv').config();
 
 var firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId
+    // apiKey: process.env.API_KEY,
+    // authDomain: process.env.AUTH_DOMAIN,
+    // projectId: process.env.PROJECT_ID,
+    // storageBucket: process.env.STORAGE_BUCKET,
+    // messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    // appId: process.env.APP_ID
+    apiKey: "AIzaSyCm8WmMt2CdmK_1WEPcdB4grGyAce8I99s",
+    authDomain: "pixelgram-80e0b.firebaseapp.com",
+    projectId: "pixelgram-80e0b",
+    storageBucket: "pixelgram-80e0b.appspot.com",
+    messagingSenderId: "722674977769",
+    appId: "1:722674977769:web:1e7b7a9082a8be73d89978"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
