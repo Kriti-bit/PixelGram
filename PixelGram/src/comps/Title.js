25b77572715fce2f9608d47838b1d61220939891
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-07-31 00:09:42 +0530

    gh-pages

--- PixelGram/src/comps/Title.js
+++ PixelGram/src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>PixelGram</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Share your pictres with the world</p>
     </div>
   )
 }
