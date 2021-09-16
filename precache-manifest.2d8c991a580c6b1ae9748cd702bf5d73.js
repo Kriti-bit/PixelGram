378ac8ba0212ef28429531004b16c7ec77d61efb
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-09-16 11:04:10 +0530

    Updates

---
+++ precache-manifest.2d8c991a580c6b1ae9748cd702bf5d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7bfce85c3435eed65b00acf3836011c",
+    "url": "/PixelGram/index.html"
+  },
+  {
+    "revision": "cd186e7beb4167a87a99",
+    "url": "/PixelGram/static/css/main.b4db3970.chunk.css"
+  },
+  {
+    "revision": "6aa53c300b6167e0fc95",
+    "url": "/PixelGram/static/js/2.27c43b6e.chunk.js"
+  },
+  {
+    "revision": "f13b8ee62297cdeb55e53ad5d3f0bd7f",
+    "url": "/PixelGram/static/js/2.27c43b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd186e7beb4167a87a99",
+    "url": "/PixelGram/static/js/main.16d094f3.chunk.js"
+  },
+  {
+    "revision": "3c1a43f0068a085270b2",
+    "url": "/PixelGram/static/js/runtime-main.882481c6.js"
+  }
+]);
