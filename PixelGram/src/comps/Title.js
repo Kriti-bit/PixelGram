8bfcff49efa2da51f88a1ca96ba3b20e827712e7
iamkriti <69788174+Kriti-bit@users.noreply.github.com>
2021-09-16 11:02:29 +0530

    removed typo

--- PixelGram/src/comps/Title.js
+++ PixelGram/src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>PixelGram</h1>
       <h2>Your Pictures</h2>
-      <p>Share your pictres with the world</p>
+      <p>Share your pictures with the world</p>
     </div>
   )
 }
